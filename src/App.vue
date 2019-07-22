3d7d38c2323871fc0d735cd7627b796de4530a2c
Ivan Senchukov <Ivan.Senchukov@rt.ru>
2019-07-22 15:57:10 +0300

    vue_crash_todolist  - example of 'data()' method;  - data binding in template;

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    {{msg}}
   </div>
 </template>
 
@@ -9,6 +9,11 @@
 export default {
   name: 'app',
   components: {
+  },
+  data() {
+    return {
+      msg: 'Hello Message'
+    }
   }
 }
 </script>
