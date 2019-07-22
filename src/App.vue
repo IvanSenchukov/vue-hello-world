3a8619e31029edb6342c5d967f07129e51b4e241
Ivan Senchukov <Ivan.Senchukov@rt.ru>
2019-07-22 17:47:07 +0300

    vue_crash_todolist  - add hardcoded 'todos' data;  - add some basic styles in App.vue;

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    {{msg}}
-  </div>
+    <div id="app">
+
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
-  },
-  data() {
-    return {
-      msg: 'Hello Message'
+    export default {
+        name: 'app',
+        components: {},
+        data() {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        title: "Todo One",
+                        completed: false
+                    },
+                    {
+                        id: 2,
+                        title: "Todo Two",
+                        completed: true
+                    },
+                    {
+                        id: 3,
+                        title: "Todo Three",
+                        completed: false
+                    }
+                ]
+            }
+        }
     }
-  }
-}
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.4;
+    }
 </style>
