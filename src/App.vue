de9460419c61bce7ad7ebe09dd580421207c945e
Ivan Senchukov <Ivan.Senchukov@rt.ru>
2019-07-22 17:48:08 +0300

    vue_crash_todolist  - example of creating and importing Todos component;

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
     <div id="app">
-
+        <Todos/>
     </div>
 </template>
 
 <script>
+    import Todos from './components/Todos';
 
     export default {
         name: 'app',
-        components: {},
+        components: {Todos: Todos},
         data() {
             return {
                 todos: [
